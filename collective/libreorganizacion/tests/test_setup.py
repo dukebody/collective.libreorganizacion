5d02152e6c4570910551da03984826d5d6b02064
Israel Saeta Pérez <dukebody@gmail.com>
2010-06-05 22:14:38 +0200

    Add customized topic summary view.

--- collective/libreorganizacion/tests/test_setup.py
+++ collective/libreorganizacion/tests/test_setup.py
@@ -1,4 +1,5 @@
 from base import LibreOrganizacionTestCase
+from Products.CMFCore.utils import getToolByName
 
 class TestProductInstall(LibreOrganizacionTestCase):
 
@@ -18,7 +19,14 @@ class TestProductInstall(LibreOrganizacionTestCase):
     def testOnlyManagerCanAddKeywords(self):
         sp = self.portal.portal_properties.site_properties
         self.failUnlessEqual(sp.getProperty('allowRolesToAddKeywords'), ('Manager',))
-                            
+
+    def testCustomTopicView(self):
+        "Test that the collection type have an extra custom view method."
+        pt = getToolByName(self.portal, 'portal_types')
+        topic_type = pt.Topic
+        self.failUnless('custom_folder_summary_view' in \
+                        topic_type.getProperty('view_methods'))
+                    
 class TestInstantiation(LibreOrganizacionTestCase):
     
     def testCreateLibreOrganizacion(self):
