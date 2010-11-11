734ef2a39054846b45d18ad259e197b445efb243
Israel Saeta Pérez <dukebody@gmail.com>
2010-11-11 15:53:42 +0100

    Check that electors can create proposals.

--- collective/libreorganizacion/tests.py
+++ collective/libreorganizacion/tests.py
@@ -7,6 +7,9 @@ from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import applyProfile
 
+from plone.app.testing import setRoles
+from plone.app.testing import login, logout
+from plone.app.testing import TEST_USER_NAME
 
 from zope.configuration import xmlconfig
 
@@ -27,11 +30,23 @@ class LibreOrganizacionLayer(PloneSandboxLayer):
         # install into the Plone site
         applyProfile(portal, 'collective.libreorganizacion:default')
 
+        setRoles(portal, TEST_USER_NAME, ['Manager', 'Member'])
+        login(portal, TEST_USER_NAME)
+        portal.invokeFactory('Folder', 'folder1')
+        folder = portal.folder1
+        setRoles(portal, TEST_USER_NAME, ['Member'])
+
+        acl_users = getToolByName(portal, 'acl_users')
+        acl_users.userFolderAddUser('elector', 'elector', ['Member'], [])
+        folder.manage_setLocalRoles('elector', ['Member', 'Contributor', 'Elector'])
+        self['folder'] = folder
+
 
 LIBREORGANIZACION_FIXTURE = LibreOrganizacionLayer()
 
 LIBREORGANIZACION_INTEGRATION_TESTING = IntegrationTesting(bases=(LIBREORGANIZACION_FIXTURE,), name="LibreOrganizacion:Integration")
 LIBREORGANIZACION_FUNCTIONAL_TESTING = FunctionalTesting(bases=(LIBREORGANIZACION_FIXTURE,), name="LibreOrganizacion:Functional")
+
 
 class TestProductInstall(unittest.TestCase):
     layer = LIBREORGANIZACION_INTEGRATION_TESTING
@@ -52,10 +67,15 @@ class TestProductInstall(unittest.TestCase):
         self.failUnless('custom_folder_summary_view' in \
                         topic_type.getProperty('view_methods'))
 
-
     def testElectorRole(self):
         """Comprobar que existe el rol 'Elector'"""
         self.assertTrue('Elector' in self.layer['portal'].valid_roles())
+
+    def testElectorCanCreateProposals(self):
+        """Comprobar que un Elector puede crear propuestas"""
+        portal = self.layer['portal']
+        login(portal, 'elector')
+        self.layer['folder'].invokeFactory('collective.libreorganizacion.proposal', 'proposal1')
 
 
 def test_suite():
