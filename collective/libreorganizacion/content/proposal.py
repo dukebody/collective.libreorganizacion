e3c003d754b9110b0a5739b2d2940cba3b113fea
Israel Saeta Pérez <dukebody@gmail.com>
2010-11-11 16:14:37 +0100

    Refactor vocabularies.

--- collective/libreorganizacion/content/proposal.py
+++ collective/libreorganizacion/content/proposal.py
@@ -2,6 +2,7 @@
 
 # CMF and Zope imports
 from zope import schema
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from zope.interface import Interface
 
 from plone.app.textfield import RichText
@@ -10,22 +11,21 @@ from plone.namedfile.field import NamedImage
 from collective.libreorganizacion import _
 
 
-# INICIO VOCABULARIES A ELIMINAR
 # Vocabularies
-TOPICS = [
-  ('derechoshumanos', 'Derechos Humanos'),
-  ('derechosanimales', 'Derechos de los animales'),
-  ('ecologia', 'Ecología y desarrollo sostenible'),
-  ('etc', 'Etc.'),
-    ]
-SCOPES = [
-  ('global', 'Global'),
-  ('international', 'International'),
-  ('national', 'National'),
-  ('regional', 'Regional'),
-  ('provincial', 'Provincial'),
-  ('local', 'Local'),
-]
+TOPICS = SimpleVocabulary([
+    SimpleTerm(value='derechoshumanos', title=_(u'Derechos Humanos')),
+    SimpleTerm(value='derechosanimales', title=_(u'Derechos de los animales')),
+    SimpleTerm(value='ecologia', title=_(u'Ecología y desarrollo sostenible')),
+    ])
+
+SCOPES = SimpleVocabulary([
+    SimpleTerm(value='global', title=_(u'Global')),
+    SimpleTerm(value='international', title=_(u'Internacional')),
+    SimpleTerm(value='national', title=_(u'Nacional')),
+    SimpleTerm(value='regional', title=_(u'Regional')),
+    SimpleTerm(value='provincial', title=_(u'Provincial')),
+    SimpleTerm(value='local', title=_(u'Local')),
+    ])
 
 
 class IProposal(Interface):
@@ -47,13 +47,13 @@ class IProposal(Interface):
     topics = schema.Choice(
         title=_(u'Temas'),
         description=_(u'Temas de la propuesta'),
-        values=TOPICS,
+        vocabulary=TOPICS,
         )
 
     scope = schema.Choice(
         title=_(u'Ámbito'),
         description=_(u'Ámbito territorial de la propuesta'),
-        values=SCOPES,
+        vocabulary=SCOPES,
         )
 
     #TODO: hacer Choice con vocabulario de países
