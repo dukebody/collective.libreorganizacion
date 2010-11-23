995c5dc32ae75338ef450b014f062f080a32c069
Israel Saeta Pérez <dukebody@gmail.com>
2010-11-23 23:07:31 +0100

    Show poll result after the voting ends.

--- collective/libreorganizacion/subscribers.py
+++ collective/libreorganizacion/subscribers.py
@@ -4,12 +4,11 @@ from Products.DCWorkflow.interfaces import IAfterTransitionEvent
 from Products.CMFPlone.utils import getToolByName
 
 
-def proposal_transition_dispatch(obj, event):
+def proposal_transition_dispatch(proposal, event):
     """Handles actions that should be made when a proposal does transitions.
     """
 
-    proposal = obj
-
+    # create a skeleton poll when the proposal hits the plenary
     if IAfterTransitionEvent.providedBy(event) and\
         event.old_state.id == 'pending' and event.new_state.id == 'plenary':
         proposal.invokeFactory('PlonePopoll', 'poll')
@@ -26,13 +25,47 @@ def proposal_transition_dispatch(obj, event):
         poll.update()
 
 
+    # start the poll when the voting on the proposal starts
     if IAfterTransitionEvent.providedBy(event) and\
         event.old_state.id == 'plenary' and event.new_state.id == 'voting':
-        wft = getToolByName(obj, 'portal_workflow')
+        wft = getToolByName(proposal, 'portal_workflow')
 
         poll = proposal.poll
 
-        # publish draft proposal
+        # start poll voting
         status = wft.getStatusOf('collective.libreorganizacion.poll_workflow', poll)
         if status['review_state'] == 'draft':
-            wft.doActionFor(poll, 'publish')
+            wft.doActionFor(poll, 'start')
+
+
+    # stop the poll when the voting on the proposal ends
+    if IAfterTransitionEvent.providedBy(event) and\
+        event.old_state.id == 'voting' and event.new_state.id == 'archived':
+        wft = getToolByName(proposal, 'portal_workflow')
+
+        poll = proposal.poll
+
+        # close poll voting
+        status = wft.getStatusOf('collective.libreorganizacion.poll_workflow', poll)
+        if status['review_state'] == 'voting':
+            wft.doActionFor(poll, 'close')
+
+
+def poll_transition_dispatch(poll, event):
+    """Handles actions that should be made when a poll does transitions.
+    """
+
+    # enable poll after voting starts
+    if IAfterTransitionEvent.providedBy(event) and\
+        event.old_state.id == 'draft' and event.new_state.id == 'voting':
+
+        poll.setEnabled(True)
+        poll.update()
+
+
+    # disable poll after voting ends
+    if IAfterTransitionEvent.providedBy(event) and\
+        event.old_state.id == 'voting' and event.new_state.id == 'closed':
+
+        poll.setEnabled(False)
+        poll.update()
